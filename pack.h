d1136144b7e06de0961bb483971b7ca1a79faec5
josebenjumeda98 <jose.benjumeda@estudiante.uam.es>
2019-11-07 19:53:26 +0100

    boniato

---
+++ pack.h
@@ -0,0 +1,15 @@
+#ifndef TRANSFORMA_H
+#define TRANSFORMA_H
+
+#define
+
+#include <stdio.h>
+
+typedef struct _Pack
+{
+  char nombre[50];
+  int estados[NUM_ESTADOS]
+} Pack;
+
+
+#endif
---
+++ transforma.h
@@ -0,0 +1,10 @@
+#ifndef TRANSFORMA_H
+#define TRANSFORMA_H
+
+#include <stdio.h>
+
+#include "afnd.h"
+
+
+
+#endif
